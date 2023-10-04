fc3c436beed0c4ffc07efe35c228e9db5f1c48a8
georgemoodier <84156418+georgemoodier@users.noreply.github.com>
2023-10-04 21:45:16 +0330

    Update armcord.rb

--- Casks/armcord.rb
+++ Casks/armcord.rb
@@ -3,12 +3,12 @@ cask "armcord" do
   arch arm: "-arm64", intel: ""
 
 
-  version "3.2.4"
+  version "3.2.4-libwebp"
   sha256 arm: "37d1d694912f18c32a2db63111fa969fcfbce6c8e0a25811fde1daee5f77d100 ",
 intel: "b078b8370a399a7aead2ceac12e4fd9ce2ab2e592ab2db83791943d08f506b16"
 
 
-  url "https://github.com/ArmCord/ArmCord/releases/download/v#{version}-libwebp/ArmCord-#{version}#{arch}.dmg",
+  url "https://github.com/ArmCord/ArmCord/releases/download/v#{version}/ArmCord-#{version.sub(%r{-libwebp}, '')}#{arch}.dmg",
       verified: "github.com/ArmCord/ArmCord/"
   name "armcord"
   desc "Custom lightweight Discord client designed to enhance your experience"
