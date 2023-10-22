2937c36cab2d04f003f16c4c7e5791f1eb50fc65
georgemoodier <84156418+georgemoodier@users.noreply.github.com>
2023-10-22 21:33:32 +0330

    fixed checksum

--- Casks/armcord.rb
+++ Casks/armcord.rb
@@ -4,8 +4,8 @@ cask "armcord" do
 
 
   version "3.2.5"
-  sha256 arm: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
-intel: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 arm: "c697a9336304fdedd429d5c990c57a783d578aad2ff19c3501297aec12338e2d",
+intel: "25676f333becaf6222ae2e1086e568656548e836d91d7ad2e6cf1723d93e7710"
 
 
   url "https://github.com/ArmCord/ArmCord/releases/download/v#{version}/ArmCord-#{version}#{arch}.dmg",
